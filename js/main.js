483aeb0c0aed16587a1a889429d785e9d86ac901
antoineafonin <antoineafonin@gmail.com>
2024-11-08 19:54:40 -0500

    section hero is done

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const users = {
+    user1: {
+        name: "Anton",
+        age: 17,
+    },
+    user2: {
+        name: "John",
+        age: 12,
+    },
+    user3: {
+        name: "Tom",
+        age: 20,
+    },
+};
